724d90675bc087827d6a8761283a2fc2e1df95a8
elenamauri <146062101+elenamauri@users.noreply.github.com>
2024-05-23 17:42:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const button = document.querySelector('.cv');
 
-  button.addEventListener('mouseenter', () => {
+// Funzione per rilevare se il dispositivo è mobile
+  function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+  }
+  
+ // Se è un dispositivo mobile, imposta il testo del pulsante
+  if (isMobileDevice()) {
     button.textContent = 'Download my CV!';
-  });
+    button.classList.add('mobile');
+  } else {
+    button.addEventListener('mouseenter', () => {
+      button.textContent = 'Download my CV!';
+    });
 
-  button.addEventListener('mouseleave', () => {
-    button.textContent = 'Do you want to know more?';
-  });
+    button.addEventListener('mouseleave', () => {
+      button.textContent = 'Do you want to know more?';
+    });
+  }
 });
